ee0ad98275774cf5f12a1f79289f2f8e9b204125
Mabelle Cornelius <mabelinegcornelius@gmail.com>
2021-07-26 15:43:53 -0700

    Experiment design done, but not pretty

--- draw/urls.py
+++ draw/urls.py
@@ -5,6 +5,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<str:room_name>/', views.room, name='room'),
+    path('layouts/', views.layouts, name='layouts'),
+    path('layouts/layout1/', views.layout1, name='layout1'),
+    path('layouts/layout2/', views.layout2, name='layout2'),
+    path('small/', views.small, name='small'),
+    # path('<str:room_name>/', views.room, name='room'),
 
 ]
--- draw/views.py
+++ draw/views.py
@@ -4,7 +4,19 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'draw/index.html')
 
-def room(request, room_name):
-    return render(request, 'draw/room.html', {
-        'room_name': room_name
-    })
+def layouts(request):
+    return render(request, 'draw/layouts.html')
+
+def layout1(request):
+    return render(request, 'draw/layout1.html')
+
+def layout2(request):
+    return render(request, 'draw/layout2.html')
+
+def small(request):
+    return render(request, 'draw/small.html')
+
+# def room(request, room_name):
+#     return render(request, 'draw/room.html', {
+#         'room_name': room_name
+#     })
